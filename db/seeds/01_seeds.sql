8c684922568b9f793d4466160d37f16e7c06c84b
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-10-12 00:21:08 -0700

    Create users seeds

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -1,3 +1,23 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Tables seeds here (Example)
+
+DELETE FROM favourites;
+DELETE FROM points;
+DELETE FROM maps;
+DELETE FROM users;
+select setval ('favourites_id_seq', 1, false);
+select setval ('points_id_seq', 1, false);
+select setval ('maps_id_seq', 1, false);
+select setval ('users_id_seq', 1, false);
+
+INSERT INTO users (
+name, email, password)
+VALUES (
+'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (
+name, email, password)
+VALUES (
+'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (
+name, email, password)
+VALUES (
+'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
