5119bafdf61786a8f18708f156fdc33e3a5b92da
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-10-14 12:47:47 -0700

    Create user login route

--- routes/users-api.js
+++ routes/users-api.js
@@ -22,6 +22,42 @@ router.get('/', (req, res) => {
     });
 });
 
+/**
+ * Check if a user exists with a given username and password
+ * @param {String} email
+ * @param {String} password encrypted
+ */
+const login = function(email, password) {
+  return userQueries.getUserWithEmail(email)
+  .then(user => {
+    if (bcrypt.compareSync(password, user.password)) {
+      return user;
+    }
+    return null;
+  });
+};
+
+// login
+router.post('/login', (req, res) => {
+  // const {email, password} = req.body;
+  const {email, password} = {email:'at@example.com' ,password: 'password'};
+  login(email, password)
+    .then(user => {
+      if (!user) {
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
+      res.send({user: {name: user.name, email: user.email, id: user.id}});
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error:err.message});
+    });
+  });
+
+// create a new user
 router.post('/', (req, res) => {
   const user = req.body;
   user.password = bcrypt.hashSync(user.password, 12);
