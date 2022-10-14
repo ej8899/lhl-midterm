e615ed5f756d71f1626bbf6c34985411082434ad
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-10-14 11:55:16 -0700

    Add comments to queries

--- db/queries/widgets.js
+++ db/queries/widgets.js
@@ -1,5 +1,13 @@
 const query = require('../connection');
 
+
+// favourites
+
+/**
+ * Add a favourite to the database.
+ * @param {{}}} user_id, map_id.
+ * @return {Promise<{}>} A promise to the property.
+ */
 const addFavourite = (favourite) => {
   const queryValues = [
     favourite.map_id,
@@ -9,14 +17,30 @@ const addFavourite = (favourite) => {
   return query(`INSERT INTO favourites (map_id, user_id) VALUES ($1, $2) RETURNING *;`, queryValues, result => result.rows);
 };
 
+// maps
+
+/**
+ * Get a single map from the database given oener_id.
+ * @param {String} owner_id.
+ * @return {Promise<{}>} A promise to the map.
+ */
 const getMapsWithOwnerId = (id) => {
   return query('SELECT * FROM maps WHERE owner_id = $1;', [id], result => result.rows);
 };
 
+/**
+ * Get maps those are not private from the database.
+ * @return {Promise<{}>} A promise to the map.
+ */
 const getAllNoPrivateMaps = () => {
   return query('SELECT * FROM maps WHERE is_private = false ORDER BY name ASC;', [], result => result.rows);
 };
 
+/**
+ * Add a map to the database.
+ * @param {{}}} map. An object containing all of the map details.
+ * @return {Promise<{}>} A promise to the map.
+ */
 const addMap = (map) => {
   const queryValues = [
     map.name,
@@ -35,10 +59,22 @@ const addMap = (map) => {
   `, queryValues, result => result.rows);
 };
 
+// points
+
+/**
+ * Get points from database given map_id.
+ * @param {string}} map_id.
+ * @return {Promise<{}>} A promise to the point.
+ */
 const getPointsWithMapId = (id) => {
   return query('SELECT * FROM points WHERE map_id = $1;', [id], result => result.rows);
 };
 
+/**
+ * Add a point to the database.
+ * @param {{}}} point. An object containing all of the point details.
+ * @return {Promise<{}>} A promise to the point.
+ */
 const addPoint = (point) => {
   const queryValues = [
     point.title,
